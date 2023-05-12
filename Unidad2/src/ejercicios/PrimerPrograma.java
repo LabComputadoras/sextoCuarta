c311430076e2fcb7ea29e0800bb5c403e3ba6d15
Dario Marin Roth <laboratoriodecomputadoraselectronicas64@epet14.edu.ar>
2023-05-12 18:58:40 -0300

    Primer programa: Declaración de la clase y del método main.

---
+++ Unidad2/src/ejercicios/PrimerPrograma.java
@@ -0,0 +1,8 @@
+package ejercicios;
+
+public class PrimerPrograma {                   // Declaración de la clase.
+    public static void main(String[] args) {    // Declaración del método main.
+
+
+    }                                           // Fin del método main.
+}                                               // Fin de la clase.
