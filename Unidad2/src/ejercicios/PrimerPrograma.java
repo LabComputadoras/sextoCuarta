8f5cb34abd52d6e8683ac56c09748dafd31e113b
Dario Marin Roth <laboratoriodecomputadoraselectronicas64@epet14.edu.ar>
2023-05-12 19:19:10 -0300

    Variables

--- Unidad2/src/ejercicios/PrimerPrograma.java
+++ Unidad2/src/ejercicios/PrimerPrograma.java
@@ -1,9 +1,25 @@
 package ejercicios;
 
-public class PrimerPrograma {                   // Declaración de la clase.
-    public static void main(String[] args) {    // Declaración del método main.
-        System.out.print("Hola mundo");         // Imprimir un mensajito.
+public class PrimerPrograma {
+    public static void main(String[] args) {
 
-    }                                           // Fin del método main.
+        int numeroEntero;   // Una variable para guardar un número entero.
+        float decimal;      // Una variable para guardar un número con parte decimal.
+        String texto;       // Una variable para guardar texto.
+        boolean bandera;    // Una variable para guardar un booleano.
+        char caracter;      // Una variable para guardar un caracter.
 
-}                                               // Fin de la clase.
+        numeroEntero = 15;  // Asignamos un número a la variable entera.
+        decimal = 1.2f;     // Asignamos un número a la variable decimal.
+        texto = "hola!";    // Asignamos un texto a la variable string.
+        bandera = true;     // Asignamos un boolean a la variable boolean.
+        caracter = 'A';     // Asignamos un caracter a la variable char.
+
+        System.out.println(numeroEntero);
+        System.out.println(decimal);
+        System.out.println(texto);
+        System.out.println(bandera);
+        System.out.println(caracter);
+
+    }
+}
