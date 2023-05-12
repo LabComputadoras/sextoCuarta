4edb8b11869ef2f5dc7c664a156f08ef490d6506
Dario Marin Roth <laboratoriodecomputadoraselectronicas64@epet14.edu.ar>
2023-05-12 19:03:50 -0300

    Imprimiendo un mensaje

--- Unidad2/src/ejercicios/PrimerPrograma.java
+++ Unidad2/src/ejercicios/PrimerPrograma.java
@@ -2,7 +2,8 @@ package ejercicios;
 
 public class PrimerPrograma {                   // Declaración de la clase.
     public static void main(String[] args) {    // Declaración del método main.
-
+        System.out.print("Hola mundo");         // Imprimir un mensajito.
 
     }                                           // Fin del método main.
+
 }                                               // Fin de la clase.
