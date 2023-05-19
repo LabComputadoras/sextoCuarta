79f5cf6f1843aaddb5bedd92fb47c24165dd85eb
Dario Marin Roth <laboratoriodecomputadoraselectronicas64@epet14.edu.ar>
2023-05-19 14:13:32 -0300

    Ejemplo: probamos una condición verdadera.

--- Unidad2/src/ejercicios/Decisiones.java
+++ Unidad2/src/ejercicios/Decisiones.java
@@ -3,15 +3,13 @@ package ejercicios;
 public class Decisiones {
     public static void main(String[] args) {
 
+        int edad = 19;  // Se declara una variable entera que inicialmente vale 19.
 
-        if (aca hay que poner una condición ){     // Comienzo del if.
-
-        }                                          // Fin del if.
-
-        /*
-         Las líneas de código dentro del if solo se ejecutan si la condición es verdadera.
-         Si la condición es falsa, ignora todo el contenido del if y continua con la ejecución normal del código.
-        */
-
+        if (edad > 18){
+            System.out.println("Es mayor de edad, puede ingresar :)");
+        }
+        System.out.println("Llegamos al final del código.");
     }
 }
+
+// Al cumplirse la condición del if, en la consola veremos tanto el mensaje de dentro del if como el de fuera.
