b54de68e87a8147384608cb51328d9544382d8cb
Dario Marin Roth <laboratoriodecomputadoraselectronicas64@epet14.edu.ar>
2023-05-19 13:48:39 -0300

    Creando un objeto Scanner (Teclado)

---
+++ Unidad2/src/ejercicios/Teclado.java
@@ -0,0 +1,9 @@
+package ejercicios;
+
+import java.util.Scanner;       // Importamos la clase Scanner para poder crear objetos Scanner.
+
+public class Teclado {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);   // Creamos un objeto Scanner que nos ayudará a realizar entradas manuales.
+    }
+}
