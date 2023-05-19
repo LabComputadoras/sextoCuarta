46f00a50acb993dfa7181be4f08deac5e94e2b51
Dario Marin Roth <laboratoriodecomputadoraselectronicas64@epet14.edu.ar>
2023-05-19 14:08:22 -0300

    Presentando la setencia if

---
+++ Unidad2/src/ejercicios/Decisiones.java
@@ -0,0 +1,17 @@
+package ejercicios;
+
+public class Decisiones {
+    public static void main(String[] args) {
+
+
+        if (aca hay que poner una condición ){     // Comienzo del if.
+
+        }                                          // Fin del if.
+
+        /*
+         Las líneas de código dentro del if solo se ejecutan si la condición es verdadera.
+         Si la condición es falsa, ignora todo el contenido del if y continua con la ejecución normal del código.
+        */
+
+    }
+}
