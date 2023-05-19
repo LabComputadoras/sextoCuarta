7af710bd7b8ed8df2dcf549c45046b0caa4d358c
Dario Marin Roth <laboratoriodecomputadoraselectronicas64@epet14.edu.ar>
2023-05-19 14:40:29 -0300

    Presentando while

---
+++ Unidad2/src/ejercicios/Repeticiones.java
@@ -0,0 +1,19 @@
+package ejercicios;
+
+public class Repeticiones {
+    public static void main(String[] args) {
+
+        while(aca hay que poner una condición){     //  Comienzo del while.
+
+
+        }                                           // Fin del while.
+
+    }
+}
+
+/*
+    While es una estructura que repetirá todas las líneas escritas en su interior siempre y cuando la condición que
+    hayamos especificado se cumpla.
+    Si en un primer momento, la condición no se cumple, se ignorará el contenido del while y se continuará con la ejecución
+    normal del código.
+ */
