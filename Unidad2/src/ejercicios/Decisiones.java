fa3ceea0fa98a0b5c984f96d2124ac05d65189b2
Dario Marin Roth <laboratoriodecomputadoraselectronicas64@epet14.edu.ar>
2023-05-19 14:30:12 -0300

    Presentando "else"

--- Unidad2/src/ejercicios/Decisiones.java
+++ Unidad2/src/ejercicios/Decisiones.java
@@ -6,16 +6,22 @@ public class Decisiones {
     public static void main(String[] args) {
 
         int edad;
-        Scanner ent = new Scanner(System.in);                           // Creamos un objeto Scanner para las entradas manuales.
+        Scanner ent = new Scanner(System.in);
 
-        System.out.println("Por favor, ingrese su edad.");              // Instrucción para el usuario.
-        edad = ent.nextInt();                                           // Leemos la edad del usuario y la guardamos en la variable edad.
+        System.out.println("Por favor, ingrese su edad.");
+        edad = ent.nextInt();
 
         if (edad > 18){
             System.out.println("Es mayor de edad, puede ingresar :)");
-        }
+        } else {    // Comienzo del else.
+
+        }           // Fin del else.
+
         System.out.println("Llegamos al final del código.");
     }
 }
 
-// Ahora, dependiendo de la edad que digite el usuario, las lineas del if podrían o no ejecutarse.
+/*
+ Else es una estructura opcional que podemos añadir si deseeamos programar un coportamiento que solamente se ejecutará
+ si la condición resulta falsa.
+*/
