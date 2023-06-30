aa79b7afc132e28404c1da7a6e9050d37e61655a
Dario Marin Roth <laboratoriodecomputadoraselectronicas64@epet14.edu.ar>
2023-06-30 18:52:15 -0300

    Construimos una función para imprimir vectores.

--- Unidad 3/src/explicaciones/Vectores.java
+++ Unidad 3/src/explicaciones/Vectores.java
@@ -2,18 +2,17 @@ package explicaciones;
 
 public class Vectores {
     public static void main(String[] args) {
-
-        int[] vector;           // Declaración del vector.
-        int[] segundoVector = {1, 2, 3, 4};
-        vector = new int[4];    // inicializando el vector en 4 elementos.
-
-        vector[0] = 1;
-        vector[1] = 2;
-        vector[2] = 3;
-        vector[3] = 4;
-
-        System.out.println(vector[2]);
-        System.out.println(segundoVector[0]);
-
+        int[] vector = {1,2,3};
+        int[] segundoVector = {1, 2, 3, 4, 1 , 1, 1, 1};
+        imprimirVector(segundoVector);
+        System.out.println();
+        imprimirVector(vector);
     }
+
+    static void imprimirVector(int[] vector) {
+        for (int i = 0; i < vector.length; i++) {
+            System.out.println(vector[i]);
+        }
+    }
+
 }
