5df915873f0db838c94247b1796db9aba4201527
Dario Marin Roth <laboratoriodecomputadoraselectronicas64@epet14.edu.ar>
2023-06-30 18:38:36 -0300

    Declaramos dos vectores, le cargamos valores e imprimimos algunas coordenadas.

---
+++ Unidad 3/src/explicaciones/Vectores.java
@@ -0,0 +1,19 @@
+package explicaciones;
+
+public class Vectores {
+    public static void main(String[] args) {
+
+        int[] vector;           // Declaración del vector.
+        int[] segundoVector = {1, 2, 3, 4};
+        vector = new int[4];    // inicializando el vector en 4 elementos.
+
+        vector[0] = 1;
+        vector[1] = 2;
+        vector[2] = 3;
+        vector[3] = 4;
+
+        System.out.println(vector[2]);
+        System.out.println(segundoVector[0]);
+
+    }
+}
