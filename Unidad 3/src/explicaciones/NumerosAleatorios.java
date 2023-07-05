05b3129085fc8642c9ed856d06b66002412c3244
Dario Marin Roth <laboratoriodecomputadoraselectronicas64@epet14.edu.ar>
2023-07-05 18:59:27 -0300

    Probamos la función random

--- Unidad 3/src/explicaciones/NumerosAleatorios.java
+++ Unidad 3/src/explicaciones/NumerosAleatorios.java
@@ -3,7 +3,12 @@ package explicaciones;
 public class NumerosAleatorios {
     public static void main(String[] args) {
 
-        System.out.println(Math.random());
+        int a = 2;
+        int b = 1;
+        double r;
+
+        r = Math.random() * a + b;
+        System.out.println(r);
 
     }
 }
