38f2f864c03c411e3c0700089b3f84d6aef60488
Dario Marin Roth <laboratoriodecomputadoraselectronicas64@epet14.edu.ar>
2023-07-05 19:20:25 -0300

    Probamos la función vectorAleatorio()

--- Unidad 3/src/explicaciones/NumerosAleatorios.java
+++ Unidad 3/src/explicaciones/NumerosAleatorios.java
@@ -1,19 +1,13 @@
 package explicaciones;
 
-import funciones.Funciones.*;
-
-import static funciones.Funciones.enteroAleatorio;
-import static funciones.Funciones.imprimirVector;
+import static funciones.Funciones.*;
 
 public class NumerosAleatorios {
     public static void main(String[] args) {
         int max = 20;
         int min = 10;
-        int[] vectorAleatorio = new int[30];
-
-        for (int i = 0; i < vectorAleatorio.length; i++) {
-            vectorAleatorio[i] = enteroAleatorio(min,max);
-        }
+        int dim = 30;
+        int[] vectorAleatorio = vectorAleatorio(dim, min, max);
 
         System.out.println();
         imprimirVector(vectorAleatorio);
