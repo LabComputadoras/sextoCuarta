06fe40f5e2761564fe660cc879e428dbc3b9aecf
Dario Marin Roth <laboratoriodecomputadoraselectronicas64@epet14.edu.ar>
2023-07-05 19:01:55 -0300

    Definimos nuestro propio intervalo.

--- Unidad 3/src/explicaciones/NumerosAleatorios.java
+++ Unidad 3/src/explicaciones/NumerosAleatorios.java
@@ -2,10 +2,14 @@ package explicaciones;
 
 public class NumerosAleatorios {
     public static void main(String[] args) {
-
-        int a = 2;
-        int b = 1;
+        int max = 20;
+        int min = 10;
+        int a;
+        int b;
         double r;
+
+        a = max - min;
+        b = min;
 
         r = Math.random() * a + b;
         System.out.println(r);
